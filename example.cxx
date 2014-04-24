ee5afc5fcdda9889884969e39eae37073194e6d7
sudheeshkatkam <sudheesh.katkam@utexas.edu>
2014-04-23 23:22:38 -0500

    added code for CharCount

--- example.cxx
+++ example.cxx
@@ -1,26 +1,51 @@
 # include "mapreduce.h"
 
-class WordCount {
+#include <vector>
+#include <map>
+#include <utility>
 
-    class WordCountMap<K, V>: public Mapper {
-        map() {
+class CharCountMaster : public Master {
+    public:
 
+    void initialize() {
+        _map_container[0] = spair<char, int>('a', 1);
+        _map_container[1] = spair<char, int>('a', 1);
+        _map_container[2] = spair<char, int>('b', 1);
+        _map_container[3] = spair<char, int>('c', 1);
+    }
+
+    void finalize() const {
+        for (int i = 0; i < _result_container.size(); ++i) {
+            printf("%c:%d ", _result_container[i].first, _result_container[i].second);
         }
-    };
+    }
+};
 
-    class WordCountReduce: public Reducer {
-        reduce() {
+class CharCountMapper : public Mapper {
 
+    vector<spair<char, int> > map(vector<spair<char, int> > spairs) {
+        return spairs;
+    }
+};
+
+class CharCountReducer : public Reducer {
+
+    spair<char, int> reduce(char key, vector<int> values) {
+        int sum = 0;
+        for (int i = 0; i < values.size(); ++i) {
+            sum += values[i];
         }
-    };
-
+        return spair<char, int>(key, sum);
+    }
 };
 
 int main() {
-    int data[] = [1, 2, 3, 4];
-    int[] part = map(data, function); // MPI_Scatter
-    int result = reduce(par, function); // MPI_Reduce
-}
+    JobClient jc;
+    CharCountMaster master;
+    CharCountMapper mapper;
+    CharCountReducer reducer;
 
-// a = [1,2,3];
-// a.map(x => x+1).reduce(x,y => x*y);
+    master.initialize();
+    jc.run(master, mapper, reducer);
+    master.finalize();
+}
